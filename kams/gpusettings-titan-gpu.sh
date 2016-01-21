289b96d5f29e9bef2d1175444564d919d6133cca
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2016-01-21 17:24:06 +0100

    Added hifi3 8kHz model.

--- kams/gpusettings-titan-gpu.sh
+++ kams/gpusettings-titan-gpu.sh
@@ -1,5 +1,5 @@
 #use all cards on titan-gpu server
-#export CUDA_VISIBLE_DEVICES=0  
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # Titan Z will handle this
-export num_jobs_nnet=4
+export num_jobs_nnet=6
